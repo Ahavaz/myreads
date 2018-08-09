838e97a5be3345e6f2789c703517649fe6b6d8cf
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-09 01:27:19 -0300

    Created array to map to buttons and added shelf prop

--- src/components/ChangeShelf.js
+++ src/components/ChangeShelf.js
@@ -2,22 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './ChangeShelf.css'
 
+const buttons = [
+  {
+    value: 'read',
+    text: 'Read'
+  },
+  {
+    value: 'currentlyReading',
+    text: 'Currently Reading'
+  },
+  {
+    value: 'wantToRead',
+    text: 'Want To Read'
+  }
+]
+
 const ChangeShelf = props => (
   <div className="change-shelf">
-    <button value="read" onClick={e => props.onShowShelf(e.target.value)}>
-      Read
-    </button>
-    <button value="currentlyReading" onClick={e => props.onShowShelf(e.target.value)}>
-      Currently Reading
-    </button>
-    <button value="wantToRead" onClick={e => props.onShowShelf(e.target.value)}>
-      Want To Read
-    </button>
+    {buttons.map(button => (
+      <button
+        key={button.value}
+        value={button.value}
+        className={button.value === props.shelf ? 'selected' : ''}
+        onClick={e => props.onShowShelf(e.target.value)}
+      >
+        {button.text}
+      </button>
+    ))}
   </div>
 )
 
 ChangeShelf.propTypes = {
-  onShowShelf: PropTypes.func.isRequired
+  onShowShelf: PropTypes.func.isRequired,
+  shelf: PropTypes.string.isRequired
 }
 
 export default ChangeShelf

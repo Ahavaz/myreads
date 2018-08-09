4bb293b5d57e2c4695f1e54c6edb503ff361a3b4
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-09 07:11:43 -0300

    Synced state between books and searchedBooks

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -8,21 +8,53 @@ import SearchBar from './SearchBar'
 
 class SearchBooks extends Component {
   static propTypes = {
+    books: PropTypes.array.isRequired,
     onUpdateBookShelf: PropTypes.func.isRequired
   }
 
   state = {
     query: '',
-    books: []
+    searchedBooks: []
   }
 
   fetchSearchBooks = query => {
     BooksAPI.search(query)
       .then(
-        books => query === this.state.query.trim() && this.setState({ books })
+        searchedBooks =>
+          query === this.state.query.trim() &&
+          this.setState({
+            searchedBooks:
+              (!searchedBooks.error &&
+                searchedBooks.map(searchedBook => {
+                  if (
+                    this.props.books.filter(book => book.id === searchedBook.id)
+                      .length !== 0
+                  )
+                    return this.props.books.filter(
+                      book => book.id === searchedBook.id
+                    )[0]
+
+                  return searchedBook
+                })) ||
+              searchedBooks
+          })
       )
-      .then(() => console.log('BOOKS SEARCHED', query, this.state.books))
+      .then(() =>
+        console.log('BOOKS SEARCHED', query, this.state.searchedBooks)
+      )
   }
+
+  // updateBookState = searchedBooks => {
+  //   searchedBooks.map(searchedBook => {
+  //     if (
+  //       this.props.books.filter(book => book.id === searchedBook.id).length !==
+  //       0
+  //     )
+  //       return this.props.books.filter(book => book.id === searchedBook.id)[0]
+
+  //     return searchedBook
+  //   })
+  // }
 
   formatQuery = query => query.trimStart().replace(/\s+/g, ' ')
 
@@ -32,24 +64,24 @@ class SearchBooks extends Component {
     if (query) {
       this.fetchSearchBooks(query.trim())
     } else {
-      this.setState({ books: [] })
+      this.setState({ searchedBooks: [] })
     }
   }
 
   render() {
-    const { query, books } = this.state
+    const { query, searchedBooks } = this.state
     const { onUpdateBookShelf } = this.props
 
     return (
       <div className="search-books">
         <SearchBar query={query} onUpdateQuery={this.updateQuery} />
-        {books.error ? (
+        {searchedBooks.error ? (
           <div className="search-books-empty">
             <h1>Sorry, no matches found :|</h1>
           </div>
         ) : query ? (
           <Bookshelf
-            books={books.sort(sortBy('title'))}
+            books={searchedBooks.sort(sortBy('title'))}
             onUpdateBookShelf={onUpdateBookShelf}
           />
         ) : (

d2fd3c4803e25da7424d4ebcc37d4354625a41a8
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-09 01:36:32 -0300

    Componentized SearchBar, added search API call & search result messages

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,61 +1,77 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import escapeRegEx from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import * as BooksAPI from '../utils/BooksAPI'
 import './SearchBooks.css'
 import Bookshelf from './Bookshelf'
+import SearchBar from './SearchBar'
 
 class SearchBooks extends Component {
   static propTypes = {
-    books: PropTypes.array.isRequired,
-    onChangePage: PropTypes.func.isRequired,
     onUpdateBookShelf: PropTypes.func.isRequired
   }
 
   state = {
-    query: ''
+    query: '',
+    books: []
   }
 
+  fetchSearchBooks = query => {
+    BooksAPI.search(query)
+      .then(
+        books => query === this.state.query.trim() && this.setState({ books })
+      )
+      .then(() => console.log('BOOKS SEARCHED', query, this.state.books))
+  }
+
+  formatQuery = query => query.trimStart().replace(/\s+/g, ' ')
+
   updateQuery = query => {
-    this.setState({ query: query.trim() })
+    query = this.formatQuery(query)
+    this.setState({ query })
+    if (query) {
+      this.fetchSearchBooks(query.trim())
+    } else {
+      this.setState({ books: [] })
+    }
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
   }
 
   render() {
-    let showingBooks = this.props.books
+    const { query, books } = this.state
+    const { onUpdateBookShelf } = this.props
 
-    if (this.state.query) {
-      const match = new RegExp(escapeRegEx(this.state.query), 'i')
-      showingBooks = this.props.books.filter(
-        book => match.test(book.title) || book.authors.filter(author => match.test(author)).length !== 0
-      )
-    }
-
-    showingBooks.sort(sortBy('title'))
+    // if (query) {
+    //   const match = new RegExp(escapeRegEx(query), 'i')
+    //   showingBooks = this.props.books.filter(
+    //     book => match.test(book.title) || book.authors.filter(author => match.test(author)).length !== 0
+    //   )
+    // }
 
     return (
       <div className="search-books">
-        <div className="search-books-bar">
-          <button className="close-search" onClick={this.props.onChangePage}>
-            Close
-          </button>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={e => this.updateQuery(e.target.value)}
-            />
+        <SearchBar
+          query={query}
+          onUpdateQuery={this.updateQuery}
+          onClearQuery={this.clearQuery}
+        />
+        {books.error ? (
+          <div className="search-books-empty">
+            <h1>Sorry, no matches found :|</h1>
           </div>
-        </div>
-        <Bookshelf books={showingBooks} onUpdateBookShelf={this.props.onUpdateBookShelf} />
+        ) : query ? (
+          <Bookshelf
+            books={books.sort(sortBy('title'))}
+            onUpdateBookShelf={onUpdateBookShelf}
+          />
+        ) : (
+          <div className="search-books-empty">
+            <h1>Type something above to start searching for books</h1>
+          </div>
+        )}
       </div>
     )
   }

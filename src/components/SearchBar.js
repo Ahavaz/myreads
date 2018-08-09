81a4cb923509f8c963dded7dca6556e4c413bbaa
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-09 03:45:46 -0300

    Added search input focus on clear button click

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,25 +4,35 @@ import PropTypes from 'prop-types'
 import { DebounceInput } from 'react-debounce-input'
 import './SearchBar.css'
 
-const SearchBar = props => (
-  <div className="search-bar">
-    <Link to="/" className="close-search" />
-    <DebounceInput
-      className="search-input"
-      type="text"
-      placeholder="Search by title or author"
-      value={props.query}
-      onChange={e => props.onUpdateQuery(e.target.value)}
-      debounceTimeout={300}
-      autoFocus
-    />
-    <button
-      disabled={!props.query}
-      className={props.query ? 'clear-search' : 'clear-search hidden'}
-      onClick={() => props.onUpdateQuery('')}
-    />
-  </div>
-)
+const SearchBar = props => {
+  const searchInput = React.createRef()
+
+  const clearSearch = () => {
+    searchInput.current.focus()
+    props.onUpdateQuery('')
+  }
+
+  return (
+    <div className="search-bar">
+      <Link to="/" className="close-search" />
+      <DebounceInput
+        inputRef={searchInput}
+        className="search-input"
+        type="text"
+        placeholder="Search by title or author"
+        value={props.query}
+        onChange={e => props.onUpdateQuery(e.target.value)}
+        debounceTimeout={300}
+        autoFocus
+      />
+      <button
+        disabled={!props.query}
+        className={props.query ? 'clear-search' : 'clear-search hidden'}
+        onClick={clearSearch}
+      />
+    </div>
+  )
+}
 
 SearchBar.propTypes = {
   query: PropTypes.string.isRequired,

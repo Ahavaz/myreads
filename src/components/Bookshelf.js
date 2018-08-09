10c7980f92398e5d1bd888725733d1d4e9d495f5
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-09 01:25:38 -0300

    Componentized Book

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,40 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Bookshelf.css'
+import Book from './Book'
 
 const Bookshelf = props => (
   <div className="bookshelf">
-    <div className="bookshelf-books">
-      <ol className="books-grid">
-        {props.books.map(book => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${book.imageLinks.thumbnail})`
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select value={book.shelf} onChange={e => props.onUpdateBookShelf(e.target.value, book.id)}>
-                    <option disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors.join(', ')}</div>
-            </div>
-          </li>
-        ))}
-      </ol>
-    </div>
+    <ol className="books-grid">
+      {props.books.map(book => (
+        <li key={book.id}>
+          <Book
+            id={book.id}
+            title={book.title}
+            shelf={book.shelf}
+            image={book.imageLinks}
+            authors={book.authors}
+            rating={book.averageRating}
+            onUpdateBookShelf={props.onUpdateBookShelf}
+          />
+        </li>
+      ))}
+    </ol>
   </div>
 )
 

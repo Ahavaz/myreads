6480122789253f89274f5e50d94600a7887d6afe
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-12 02:52:43 -0300

    Added toast and handleChange function

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { toast } from 'react-toastify'
 import './Book.css'
 import brokenImageIcon from '../icons/broken-img.svg'
 import BookRating from './BookRating'
@@ -12,35 +13,55 @@ const Book = ({
   authors,
   rating,
   onUpdateBookShelf
-}) => (
-  <div className="book">
-    <div className="book-top">
-      <div
-        className="book-cover"
-        style={{
-          background: `var(--primary-light-color) url(${image.thumbnail ||
-            image}) no-repeat center`,
-          backgroundSize: `${image.thumbnail ? 'cover' : 'contain'}`
-        }}
-      />
-      <div className="book-shelf-changer">
-        <select
-          value={shelf}
-          onChange={e => onUpdateBookShelf(id, e.target.value)}
-        >
-          <option disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
+}) => {
+  const handleChange = (shelfValue, shelfText) => {
+    onUpdateBookShelf(id, shelfValue)
+    toast.info(
+      shelfValue !== 'none'
+        ? `Moving book to "${shelfText}"...`
+        : `Removing book from bookshelf...`,
+      {
+        className:
+          shelfValue !== 'none' ? 'toast-move-book' : 'toast-remove-book'
+      }
+    )
+  }
+
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            background: `var(--primary-light-color) url(${image.thumbnail ||
+              image}) no-repeat center`,
+            backgroundSize: `${image.thumbnail ? 'cover' : 'contain'}`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select
+            value={shelf}
+            onChange={e =>
+              handleChange(
+                e.target.value,
+                e.target.options[e.target.selectedIndex].text
+              )
+            }
+          >
+            <option disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
       </div>
+      <BookRating rating={rating} />
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors.join(', ')}</div>
     </div>
-    <BookRating rating={rating} />
-    <div className="book-title">{title}</div>
-    <div className="book-authors">{authors.join(', ')}</div>
-  </div>
-)
+  )
+}
 
 Book.defaultProps = {
   shelf: 'none',

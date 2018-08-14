2eb3e87a7c21bd14fc77450f98037153c3998f25
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-13 23:11:40 -0300

    Wrapped App in ApolloProvider and added config functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { RestLink } from 'apollo-link-rest'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import App from './App'
 import './index.css'
 
+const authLink = setContext((_, { headers }) => {
+  if (!localStorage.token)
+    localStorage.token = Math.random()
+      .toString(36)
+      .substr(-8)
+
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.token
+    }
+  }
+})
+
+const restLink = new RestLink({
+  uri: 'https://reactnd-books-api.udacity.com', // this is your API base url
+  credentials: 'same-origin'
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(restLink),
+  cache: new InMemoryCache()
+})
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   document.querySelector('#root')
 )

d5e4c3341267bc67a551fae3d4517c47f609cfe5
Ahavaz <lucas.cesar.8@hotmail.com>
2018-08-18 00:00:47 -0300

    Changed to class component, added shelf state and removed unused props

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,43 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import './Bookshelf.css'
 import ListBooks from './ListBooks'
 import ChangeShelf from './ChangeShelf'
 
-const Bookshelf = ({
-  books,
-  shelf,
-  loading,
-  onShowShelf,
-  onUpdateBookShelf,
-  onUpdateQuery
-}) => (
-  <div className="bookshelf">
-    <header className="bookshelf-title">
-      <h1>MyReads</h1>
-    </header>
-    <ListBooks
-      books={books.filter(book => book.shelf === shelf)}
-      onUpdateBookShelf={onUpdateBookShelf}
-      loading={loading}
-    />
-    <ChangeShelf shelf={shelf} onShowShelf={onShowShelf} />
-    <Link
-      to="/search"
-      onClick={() => onUpdateQuery('')}
-      className="open-search"
-    />
-  </div>
-)
+class Bookshelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+    onUpdateBookShelf: PropTypes.func.isRequired
+  }
 
-Bookshelf.propTypes = {
-  books: PropTypes.array.isRequired,
-  shelf: PropTypes.string.isRequired,
-  loading: PropTypes.bool.isRequired,
-  onShowShelf: PropTypes.func.isRequired,
-  onUpdateBookShelf: PropTypes.func.isRequired,
-  onUpdateQuery: PropTypes.func.isRequired
+  state = {
+    shelf: 'currentlyReading'
+  }
+
+  showShelf = shelf => this.setState({ shelf })
+
+  render() {
+    const { shelf } = this.state
+    const { books, loading, onUpdateBookShelf } = this.props
+
+    return (
+      <div className="bookshelf">
+        <header className="bookshelf-title">
+          <h1>MyReads</h1>
+        </header>
+        <ListBooks
+          books={books.filter(book => book.shelf === shelf)}
+          loading={loading}
+          onUpdateBookShelf={onUpdateBookShelf}
+        />
+        <ChangeShelf shelf={shelf} onShowShelf={this.showShelf} />
+        <Link to="/search" className="open-search" />
+      </div>
+    )
+  }
 }
 
 export default Bookshelf
